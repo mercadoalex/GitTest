1990371a314073989e0507ecc58ec345b4f1e3c4
Alex Market <mercadoalex@gmail.com>
2021-05-19 07:56:55 -0500

    Initial commit from IntelliJ, clase de Java matutina,  Alex Market

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.alex;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Nos hemos conectado a Github");
+    }
+}
